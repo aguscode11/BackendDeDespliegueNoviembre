3cad3fbfdbcb20913820d1875d8d50fd0370e2c2
agus.dev <ruizagustin26@gmail.com>
2025-11-19 15:41:53 -0300

    correción dashboard 2

--- src/main.js
+++ src/main.js
@@ -5,44 +5,24 @@ import authRouter from "./routes/auth.router.js";
 import cors from 'cors'
 import taskRoutes from './routes/taskRoutes.js'; 
 
-
 connectToMongoDB()
 
 const app = express()
 
+// 🔥 CORS SIEMPRE VA PRIMERO
+app.use(cors())
 
-// CORS SIEMPRE PRIMERO
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://vercel.com/my-team-agus/frontend-de-despliegue-noviembre"
-    ],
-    credentials: true
-}));
+// Middleware para parsear JSON
+app.use(express.json())
 
-app.use(express.json());
+// Rutas
+app.use('/api/auth', authRouter)
+app.use('/api/tasks', taskRoutes)
 
-// Recién acá tus rutas
-app.use('/api/auth', authRouter);
-app.use('/api/tasks', taskRoutes);
-
-
-
+// Start server
 app.listen(
     ENVIRONMENT.PORT || 8080,
     () => {
-        console.log(`Tu servidor se esta ejecutando correctamente en el puerto ${ENVIRONMENT.PORT}`)
+        console.log(`Servidor corriendo en el puerto ${ENVIRONMENT.PORT}`)
     }
 )
-
-/* ChannelRepository.getAllByWorkspaceId() */
-
-
-/* MessagesChannelRepository.create(
-    "69021f8d1f59e8d52c3e60f8",
-    "68f8d8cc6968605f86bc6c90",
-    'hola'
-) */
-/* MessagesChannelRepository.getAllByChannelId("69021f8d1f59e8d52c3e60f8").then(
-    (messages) => console.log(messages[0])
-) */
